7e05de588c890650b73a7ded69e83ed96dde1c9e
IamdebashisD <adebashisdas626@gmail.com>
2025-01-28 17:47:08 +0530

    modify the weather API

--- student_management.py
+++ student_management.py
@@ -469,15 +469,19 @@ def login_user() -> tuple[Response, int]:
     
 
 
-@app.route('/curent_weather', methods = ['GET'])
+@app.route('/current_weather', methods = ['POST'])
 def get_weather():
     API_KEY:str = '91f939e70741477979cbd4df2994e7e9'
-    data: dict = request.get_json()
-    if not data and 'city' not in data:
+    data:dict[str, str] = request.get_json()
+    
+    if not data or not data.keys():
         return jsonify({'error': 'Please provide a city Name IN JSON format!'}), 400
     
+    CITY_NAME = data['city']
+    if not CITY_NAME or 'city' not in data:
+        return jsonify({'error': 'City name is required and cannot be empty.'}), 400
+
     try:
-        CITY_NAME = data['city']
         BASE_URL:str = f'''https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME},In&appid={API_KEY}&units=metric'''
         response = requests.get(BASE_URL)
         data: dict[str,int,float, Any] = response.json()
@@ -515,20 +519,32 @@ def get_weather():
                 country_code = data['sys']['country']
                 sunrise_time = dt.datetime.fromtimestamp(data['sys']['sunrise'] + data['timezone'])
                 sunset_time = dt.datetime.fromtimestamp(data['sys']['sunset'] + data['timezone'])
-            
+
             if data.get('name') and data.get('timezone'):
                 timezone = data['timezone']
                 city_name = data.get('name')
                 location_id = data['id']
 
-            respose = (longitude, latitude, main, description, base, temperature, feels_like, temp_min, temp_max, type_info,
-                    pressure, humidity, sea_level, ground_level, visibility, wind_speed, wind_direction, cloudiness,
-                    location_id, country_code, city_name, sunrise_time, sunset_time, timezone)
-            return jsonify({'result': respose}), 200
+            response = {
+                "City name": city_name, "Country code": country_code, "Longitude": longitude, "Latitude": latitude, "main": main,
+                "description": description, "Base": base, "temperature": f'{temperature}\u00B0C', "feels_like": feels_like, "Maximun temperature": temp_max,
+                "Minimun temperature": temp_min, "type_info": type_info, "pressure": pressure, "humidity": humidity, "sunrise time": sunrise_time,
+                "sunset time": sunset_time, "timezone": timezone, "wind speed": wind_speed, "wind direction": wind_direction,
+                "country code": country_code, "cloudiness": cloudiness, "location id": location_id, "ground level": ground_level,"see level": sea_level,
+                "visibility": visibility
+            }
+            return jsonify({'result': response}), 200
         else:
             return jsonify({'message': 'Failed to fetch'}), 404
-    except Exception as e:
-        return jsonify({'error': str(e)})
+
+    except requests.exceptions.HTTPError as http_err:
+        if response.status_code == 404:
+            return jsonify({'error': 'City not found'}), 404
+        else:
+            return jsonify({'error': f'HTTP error occured {http_err}'}), response.status_code    
+
+    except Exception as err:
+        return jsonify({'error': f'An error occured {str(err)}'})
 
 if __name__  == "__main__":
     app.run(debug = True)
